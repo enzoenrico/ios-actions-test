cd428da9d3df82fd3117b946545e645f7476bbd3
enzo <e.enrico2005@gmail.com>
2025-10-28 16:43:05 -0300

    Initial Commit

---
+++ auto-actions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  auto-actions
+//
+//  Created by Enzo Enrico on 28/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ auto-actions/auto_actionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  auto_actionsApp.swift
+//  auto-actions
+//
+//  Created by Enzo Enrico on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct auto_actionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
