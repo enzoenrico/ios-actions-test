58c933c5aeb97088e71d2132cc1618dd2dedcae7
enzo <e.enrico2005@gmail.com>
2025-10-29 18:41:07 -0300

    feat: boiler

--- auto-actions/ContentView.swift
+++ auto-actions/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Label("Hello world!", systemImage: "person")
+            Button(action:{print(self)}){
+                Text("this is a button")
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
         }
         .padding()
     }
